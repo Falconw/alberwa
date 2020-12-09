2bc05fdf9e95cec5a8addc8dbc7752b01e07a473
IT2 <i.design.and.code@gmail.com>
2020-12-09 17:03:36 +0300

    Updated the message to WA

--- pages/api/mostafeed.js
+++ pages/api/mostafeed.js
@@ -6,14 +6,24 @@ export default async function Mostafeed(req, res) {
 
   //console.log("MOSTAFEED    " + mostafeedInfoRes);
   
-  let replies = [];
+  let replies = [{
+    "message":
+      `+ ${mostafeedInfoRes[0]}
+      + ${mostafeedInfoRes[1]}
+      + ${mostafeedInfoRes[2]}
+      + ${mostafeedInfoRes[3]}
+      + ${mostafeedInfoRes[4]}
+      + ${mostafeedInfoRes[5]}`
+  }];
 
-  for (let i = 0; i < 3; i++) {
-    //replies[i] = { "message":  mostafeedInfoRes[i]}
-    replies[i] = {
-      "message": `${mostafeedInfoRes[0]} \n ${mostafeedInfoRes[1]}`
-    };
-  }
+
+  // let replies = []
+  // for (let i = 0; i < 3; i++) {
+  //   //replies[i] = { "message":  mostafeedInfoRes[i]}
+  //   replies[i] = {
+  //     "message": `${mostafeedInfoRes[0]} \n ${mostafeedInfoRes[1]}`
+  //   };
+  // }
 
   res.statusCode = 200;
   //res.json(mostafeedInfoRes);
