d9cfcd0444d28844c00346c08eadeb2ecd65557d
IT2 <i.design.and.code@gmail.com>
2020-12-14 14:48:20 +0300

    Minor Change

--- pages/api/mostafeed.js
+++ pages/api/mostafeed.js
@@ -12,12 +12,18 @@ export default async function Mostafeed(req, res) {
   const mostafeedInfoRes = await fetch(`https://www.alber.org.sa/nizam/api/mostafeed/${mostafeedID}`)
     .then(res => res.json());
   
-  const reply1 =  `+ ${mostafeedInfoRes[0]}`
-                + `\n + ${mostafeedInfoRes[1]}`
-                + `\n + ${mostafeedInfoRes[2]}`
-                + `\n + ${mostafeedInfoRes[3]}`
-                + `\n + ${mostafeedInfoRes[4]}` 
-                + `\n + ${mostafeedInfoRes[5]}`
+  let reply1;
+  if (mostafeedInfoRes[1] === undefined) {
+    reply1 = "لا يوجد معلومات عن الطلب"
+  } else {
+    reply1 = `+ ${mostafeedInfoRes[0]}`
+      + `\n + ${mostafeedInfoRes[1]}`
+      + `\n + ${mostafeedInfoRes[2]}`
+      + `\n + ${mostafeedInfoRes[3]}`
+      + `\n + ${mostafeedInfoRes[4]}`
+      + `\n + ${mostafeedInfoRes[5]}`;
+  }
+                  
   
   let replies = [{
     "message":`${reply1}`
