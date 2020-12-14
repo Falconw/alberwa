ae6beb8c27e32ac7a991274e0db4340a415743f8
IT2 <i.design.and.code@gmail.com>
2020-12-14 14:37:34 +0300

    removing logging of cooki and query & getting query from the body

--- pages/api/mostafeed.js
+++ pages/api/mostafeed.js
@@ -2,18 +2,13 @@ export default async function Mostafeed(req, res) {
   
 
   console.log("\n");
-  const toCooki = JSON.stringify(await req.cookies);
-  console.log("COOKI:  " + toCooki);
-  const toQuery = JSON.stringify(await req.query);
-  console.log("QUERY:  " + toQuery);
-  const toBody = JSON.stringify(await req.body);
-  console.log("BODY:  " + toBody);
+  const waReply = await req.body.query;
+  console.log("BODY:  " + waReply);
   console.log("\n");
 
-  // const reqreq = await req;
-  // console.log("Good luck:  " + reqreq);
 
-  const mostafeedID = "1012762967";
+  // const mostafeedID = "1012762967";
+  const mostafeedID = waReply.message;
   const mostafeedInfoRes = await fetch(`https://www.alber.org.sa/nizam/api/mostafeed/${mostafeedID}`)
     .then(res => res.json());
   
